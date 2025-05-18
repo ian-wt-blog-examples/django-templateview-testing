371436c5f82ca86e94941f8712bc58b97bddca7c
Ian Waldron <ian.waldron@scribawaldron.com>
2025-05-18 12:25:52 -0700

    add basic demo template view

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class DemoTemplateView(TemplateView):
+    template_name = 'demo/demo-template.html'
